f925a9e0eb1650ea33fe7297c01503fe21b79066
Daniyal <d.muhammad.khan@gmail.com>
2013-09-18 17:15:02 -0400

    edited doc in main java file for spelling/grammar

--- DingleMarioGame.java
+++ DingleMarioGame.java
@@ -11,15 +11,12 @@ to choose luigi's option, use the B button
 to choose mario's option, use the A button.
 
 --Controls In Game--
-tilt barell with arrow right/left arrow
+tilt barrel with arrow right/left arrow
 fire with up key
-press 1 throguh 5 to use that item if u have it
+press 1 through 5 to use that item if u have it
 fire within 7 seconds, or it will auto fire for you!
-press enetr for all options i.e. select diffulty and to replay or not
+press enter for all options i.e. select diffculty and to replay or not
 
-
---Program Capabilities--
-A lot of extra options have been implemnted into this program
 
 -Menu System Capabilities-
 1.Music (2 kinds)
@@ -32,58 +29,44 @@ A lot of extra options have been implemnted into this program
 7.option to view high score
 
 -Game capabilities-
-1.Select yoru diffulty level
+1.Select your difficulty level
     -Levels get more difficult
     -harder levels have different colour balls
     -each difficulty level has a different background
     -as user destroys more balls, the background becomes uncovered (easter egg)
-    -each difficulty level has a differrent item spawn rate (insane = 10%, hard = 15%, medium = 20%, easy = 25%)
-    -Combo system earns mroe poitns as difficulty increases and more balls break in a chain
-    -each map has differnt layout
+    -each difficulty level has a different item spawn rate (insane = 10%, hard = 15%, medium = 20%, easy = 25%)
+    -Combo system earns more points as difficulty increases and more balls break in a chain
+    -each map has different layout
     -each map is harder to win
 2.Pausing implemented (press enter in game)
 3.autoshoot function implemented (if u do not fire within 7 seconds, ball will automatically shoot)
-    -a bobomb animation will flash and explode if tiemr runs out
+    -a bombomb animation will flash and explode if tiemr runs out
     -pausing works with timer, ie.i timer stops when paused
 4.The balls will pop all balls around it, called a chain pop.
 7.win/death implemented into game
 8.records amount of turns, and outputs it as a sprite
 8.score implemented, outputs as a sprite
-    -score is nto linear, a whoel algorith as has been defined that calucaltes score based on combo, difficulty, and numebr of balls on the field
+    -score is not linear, a whole algorithm as has been defined that calculates score based on combo, difficulty, and number of balls on the field
 9.a random generator function has been added
-    -random generator is "smart" i.e. it will onyl give the balls u need once u have 10 or less balls
-10.3 differnt items have been implemented
+    -random generator is "smart" i.e. it will only give the balls u need once u have 10 or less balls
+10.3 different items have been implemented
     -mario does an animation when u get an item
 11.to the right of the score, the word "good," "great," and "excellent" will appear as u combo more balls
-12.win/lose/highscore screen all have differnt interactive screen
+12.win/lose/highscore screen all have different interactive screen
 13.option to play again
     -if not system will exit
 14.GUI implmented in high score screen to get name of winner
 15.high score records and writes to a file
-16.unique high score method implemented thatr ecords final score and number of turns it took to win
-17.ball boucnes off balls
-18.shows current ball in barell and next ball in barell
+16.unique high score method implemented that records final score and number of turns it took to win
+17.ball bounces off balls
+18.shows current ball in barrel and next ball in barrel
 19.randomly generate balls
-
--Things to improve-
-
-1.firing mechanism is nto perfect, since we are using calculate values
-2. very rarely the ball will pop a random ball on the field, because we defined the whole field as one row, but it will pop a ball that it thinks is beside it but in reality is on a differnt row
-3. chain pop pops a hexagon around it, then a hexagon aroudn each ball it comes into contact with, and thats it cannto chain more than, since we hard coded each chainpop
-    -however that is all tahst needed sinc eit is impossible to create a combinatiosn of balls on the field taht will not pop, so it is not a proble
-4. very rarely will the items malfunction, however for exmaple the mushroom will not change to colour of a ball to teh upepr right hand corner of teh new ball
-5. sometimes music will not play, due to ram issues or whatnot, just wait a sec and keep trying
-
-the logic and thiknig of up of how teh game works was equally divided between both people
-equal amoutnof work between both people
-gotta give ryan to the sick graphics though, that was all him.
 
 */
 
 
 
 
-// The "Dingle_Mario_Game" class.
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -97,7 +80,11 @@ import sun.audio.*;
 
 public class DingleMarioGame extends Applet implements Runnable, ActionListener
 {
-    // Place instance variables here
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 9121229062153180937L;
+	// Place instance variables here
     public static final Image GAME_PICS[] [] = new Image [100] [100]; //stores all pictures here
     public static final int BALL_GRID[] [] = new int [114] [4]; //ball grid defines where balls will be on the field
     public static final int ITEM_GRID[] [] = new int [5] [2]; //same things bove with items
